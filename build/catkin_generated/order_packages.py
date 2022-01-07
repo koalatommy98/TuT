25547a6c69e52dffd5e82ba86c748853f4c473c3
TUT Master <TUT>
2022-01-07 11:38:46 +0900

    first commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/miad/TuT_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/miad/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/miad/catkin_ws/devel;/opt/ros/melodic' != '' else []
